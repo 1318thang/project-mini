eb71243ee6f019c9ec6df0a9ab9781c689599dac
Dat John <dvo31666@gmail.com>
2025-10-23 21:29:09 +0700

    edit httpClient

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -4,7 +4,6 @@ import type { InternalAxiosRequestConfig } from "axios";
 const httpClient = axios.create({
     baseURL: "https://bold-wind-c8e3.1318thang.workers.dev/api/",
     // baseURL: "https://localhost:7140/api/",
-
     timeout: 10000,
 });
 // Interceptor request
